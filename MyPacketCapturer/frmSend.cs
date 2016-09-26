f23b6b1556bbc90c5e06c04f8991f7f72dda5613
Charles Davis Arvey <ca03333@ad.georgiasouthern.edu>
2016-09-26 12:48:41 -0400

    initial commit w/ lab 6 complete

---
+++ MyPacketCapturer/frmSend.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPacketCapturer
+{
+    public partial class frmSend : Form
+    {
+
+        public static int instantiations = 0;
+
+        public frmSend()
+        {
+            InitializeComponent();
+            instantiations++;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Text Files| *.txt|All Files|*.*";
+            openFileDialog1.Title = "Open Captured Packets";
+            openFileDialog1.ShowDialog();
+
+            //Check to see if a filename was given
+            if (openFileDialog1.FileName != "")
+            {
+                txtPacket.Text = System.IO.File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Text Files| *.txt|All Files|*.*";
+            saveFileDialog1.Title = "Save the Captured Packets";
+            saveFileDialog1.ShowDialog();
+
+            //Check to see if a filename was given
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.File.WriteAllText(saveFileDialog1.FileName, txtPacket.Text);
+            }
+        }
+
+        private void frmSend_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmSend_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            instantiations--;
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string stringBytes = "";
+            //Get the hex values from the file
+            foreach (string s in txtPacket.Lines)
+            {
+                stringBytes += s.Split('#')[0] + Environment.NewLine;
+            }
+
+            string[] sBytes = stringBytes.Split(new string[] { "\n", "\r\r", "\r", " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            byte[] packet = new byte[sBytes.Length];
+            int i = 0;
+            foreach (string s in sBytes)
+            {
+                packet[i] = Convert.ToByte(s, 16); 
+                i++;
+            }
+
+            try
+            {
+                frmCapture.device.SendPacket(packet);
+            } catch (Exception exp) {
+                
+            }
+                
+            
+             
+        }
+    }
+}
